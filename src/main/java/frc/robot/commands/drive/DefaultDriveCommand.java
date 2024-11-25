8271fa6c56c45c1bf12f1405f9bc4c4431638af8
Eric Faber <eric.faber@student.tdsb.on.ca>
2024-11-25 16:38:32 -0500

    changed boost button

--- src/main/java/frc/robot/commands/drive/DefaultDriveCommand.java
+++ src/main/java/frc/robot/commands/drive/DefaultDriveCommand.java
@@ -42,7 +42,7 @@ public class DefaultDriveCommand extends LoggingCommand {
 
         DriveMode driveMode = driveModeChooser.getSelected();
 
-        boolean   boost     = driverController.getRightBumper();
+        boolean   boost     = driverController.getYButton();
         boolean   shift     = driverController.getLeftBumper();
 
         SmartDashboard.putBoolean("Boost", boost);
