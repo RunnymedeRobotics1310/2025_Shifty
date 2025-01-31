8df9e6a60dad478485922f85393921c9070344d7
Sagarv-Andapally <Sagarv.andapally2@student.tdsb.on.ca>
2025-01-30 20:53:58 -0500

    placeholder for jan 31st

---
+++ src/main/java/frc/robot/commands/algae/IntakeAlgaeCommand.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.algae;
+
+import frc.robot.OperatorInput;
+import frc.robot.commands.LoggingCommand;
+
+public class IntakeAlgaeCommand extends LoggingCommand {
+
+    private final OperatorInput operatorInput;
+
+    public IntakeAlgaeCommand(OperatorInput operatorInput) {
+        this.operatorInput = operatorInput;
+
+    }
+
+    public void initialize() {
+        // placeholder
+    }
+
+    public void execute() {
+        // placeholder
+    }
+
+    public boolean isFinished() {
+        return false;
+        // placeholder
+    }
+
+    public void end() {
+        // placeholder
+    }
+
+
+
+}
+
---
+++ src/main/java/frc/robot/commands/algae/OuttakeAlgaeCommand.java
@@ -0,0 +1,34 @@
+package frc.robot.commands.algae;
+
+import frc.robot.OperatorInput;
+import frc.robot.commands.LoggingCommand;
+
+public class OuttakeAlgaeCommand extends LoggingCommand {
+
+    private final OperatorInput operatorInput;
+
+    public OuttakeAlgaeCommand(OperatorInput operatorInput) {
+        this.operatorInput = operatorInput;
+
+    }
+
+    public void initialize() {
+        // placeholder
+    }
+
+    public void execute() {
+        // placeholder
+    }
+
+    public boolean isFinished() {
+        return false;
+        // placeholder
+    }
+
+    public void end() {
+        // placeholder
+    }
+
+
+
+}
